96855f95fc7b6c8cfcb1f25d28347811400febbc
Janakitti <janakittiratana@gmail.com>
2021-05-02 16:12:24 -0400

    Adjust formatting of MovieCard

--- src/components/SearchCol/MovieCard/MovieCard.tsx
+++ src/components/SearchCol/MovieCard/MovieCard.tsx
@@ -8,7 +8,16 @@ const MovieCard = () => {
           <div className="movie-card__poster-col">
             <div className="movie-card__poster"></div>
           </div>
-          <div className="movie-card__info-col">Info</div>
+          <div className="movie-card__info-col">
+            <h1>Title</h1>
+            <h2>2021 Sci-fi</h2>
+            <p>
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has a more-or-less normal
+              distribution
+            </p>
+          </div>
         </div>
       </CustomCard>
     </>
