065c089562be545e0daa54e1af0e5f9df9d7eb31
Janakitti <janakittiratana@gmail.com>
2021-05-01 23:20:52 -0400

    Add search icon to search bar

--- src/pages/HomePage/SearchCol/SearchCol.tsx
+++ src/pages/HomePage/SearchCol/SearchCol.tsx
@@ -1,11 +1,18 @@
 import { TextField } from "@shopify/polaris";
+import { Icon } from "@shopify/polaris";
+import { SearchMajor } from "@shopify/polaris-icons";
 
 const SearchCol = () => {
   return (
     <div className="section search-col-wrapper">
       <h3>the shoppies</h3>
       <h1>Nominate</h1>
-      <TextField label="Search movies" value="hello" onChange={() => {}} />
+      <TextField
+        label="Search movies"
+        value="hello"
+        onChange={() => {}}
+        prefix={<Icon source={SearchMajor} color="base" />}
+      />
     </div>
   );
 };
