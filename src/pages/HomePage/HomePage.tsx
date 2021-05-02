d56f3c8ad5d052f62416eff8230907f52c4a182a
Janakitti <janakittiratana@gmail.com>
2021-05-02 18:06:18 -0400

    Add queryMovies

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,15 @@
 import SearchCol from "../../components/SearchCol/SearchCol";
 import NominationsCol from "../../components/NominationsCol/NominationsCol";
+import { queryMovies } from "../../services/movieservice";
+import { useEffect } from "react";
 
 const HomePage = () => {
+  useEffect(() => {
+    (async () => {
+      const movies = await queryMovies("");
+      console.log(movies);
+    })();
+  });
   return (
     <div className="home-wrapper">
       <SearchCol />
---
+++ src/services/movieservice.ts
@@ -0,0 +1,10 @@
+import { OMDB_API_URL } from "../shared/constants";
+import { IMovieResponse, IMovie } from "../shared/interfaces";
+
+export const queryMovies = async (query: string): Promise<IMovie[]> => {
+  const res = await fetch(
+    `${OMDB_API_URL}/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=star&type=movie&page=1`
+  );
+  const movies: IMovieResponse = await res.json();
+  return movies.Search;
+};
