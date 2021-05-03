5cdc595f4371aca7bbb1ba9a5d8974cb1025f5bc
Janakitti <janakittiratana@gmail.com>
2021-05-03 12:14:11 -0400

    Add ability to click NominationCard to remove nomination

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,13 +3,15 @@ interface ICardProps {
   className?: string;
   bgImage?: string;
   tint?: boolean;
+  onClick?: () => void;
 }
 
-const Card = ({ children, className, bgImage, tint }: ICardProps) => {
+const Card = ({ children, className, bgImage, tint, onClick }: ICardProps) => {
   return (
     <div
       className={`card ${className}`}
       style={{ backgroundImage: `url('${bgImage}')` }}
+      onClick={onClick}
     >
       <div className={`inner-container ${tint && "tint"}`}>{children}</div>
     </div>
--- src/components/NominationsCol/NominationCard/NominationCard.tsx
+++ src/components/NominationsCol/NominationCard/NominationCard.tsx
@@ -1,13 +1,32 @@
+import { useContext } from "react";
 import Card from "../../Card/Card";
 import { IMovieMeta } from "../../../shared/interfaces";
+import {
+  NominationsContext,
+  NominationReducerActions,
+} from "../../../pages/HomePage/HomePage";
 
 interface INominationCardProps {
   movie: IMovieMeta;
 }
 
 const NominationCard = ({ movie }: INominationCardProps) => {
+  const { dispatchNominations } = useContext(NominationsContext);
+
+  const removeNomination = () => {
+    dispatchNominations({
+      type: NominationReducerActions.REMOVE_MOVIE,
+      payload: { id: movie.imdbID },
+    });
+  };
+
   return (
-    <Card className="nomination-card__container" bgImage={movie.Poster} tint>
+    <Card
+      className="nomination-card__container"
+      bgImage={movie.Poster}
+      tint
+      onClick={removeNomination}
+    >
       <h1 className="title">{movie.Title}</h1>
       <h2 className="subtitle">{movie.Year}</h2>
     </Card>
