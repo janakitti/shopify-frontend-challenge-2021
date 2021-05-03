4a854abf31af6aa74f2cb1653d9187cf9b93df7d
Janakitti <janakittiratana@gmail.com>
2021-05-03 11:57:26 -0400

    Add tint and ellipsis on text-overflow to NominationCard

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,15 +2,16 @@ interface ICardProps {
   children: React.ReactNode;
   className?: string;
   bgImage?: string;
+  tint?: boolean;
 }
 
-const Card = ({ children, className, bgImage }: ICardProps) => {
+const Card = ({ children, className, bgImage, tint }: ICardProps) => {
   return (
     <div
       className={`card ${className}`}
       style={{ backgroundImage: `url('${bgImage}')` }}
     >
-      {children}
+      <div className={`inner-container ${tint && "tint"}`}>{children}</div>
     </div>
   );
 };
--- src/components/NominationsCol/NominationCard/NominationCard.tsx
+++ src/components/NominationsCol/NominationCard/NominationCard.tsx
@@ -7,11 +7,9 @@ interface INominationCardProps {
 
 const NominationCard = ({ movie }: INominationCardProps) => {
   return (
-    <Card className="nomination-card__container" bgImage={movie.Poster}>
-      <div className="nomination-card_tint">
-        <h1 className="title">{movie.Title}</h1>
-        <h2 className="subtitle">{movie.Year}</h2>
-      </div>
+    <Card className="nomination-card__container" bgImage={movie.Poster} tint>
+      <h1 className="title">{movie.Title}</h1>
+      <h2 className="subtitle">{movie.Year}</h2>
     </Card>
   );
 };
