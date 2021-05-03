f42595c6f01ce79362bccc590be50160919c10aa
Janakitti <janakittiratana@gmail.com>
2021-05-03 11:31:40 -0400

    Add bgImage prop to Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,14 @@
 interface ICardProps {
   children: React.ReactNode;
+  bgImage?: string;
 }
 
-const Card = ({ children }: ICardProps) => {
-  return <div className="card">{children}</div>;
+const Card = ({ children, bgImage }: ICardProps) => {
+  return (
+    <div className="card" style={{ backgroundImage: `url('${bgImage}')` }}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
