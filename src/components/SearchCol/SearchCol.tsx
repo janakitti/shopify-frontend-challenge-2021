ca92998f1d8a6f84e79b741ec93f0a1b1e10bfe6
Janakitti <janakittiratana@gmail.com>
2021-05-03 12:50:42 -0400

    Format SearchCol text

--- src/components/SearchCol/SearchCol.tsx
+++ src/components/SearchCol/SearchCol.tsx
@@ -38,8 +38,13 @@ const SearchCol = () => {
 
   return (
     <div className="section search-col-wrapper">
-      <h3>the shoppies</h3>
-      <h1>Nominate</h1>
+      <h3 className="logo-text">the shoppies</h3>
+      <h1 className="title">Nominate</h1>
+      <p className="body">
+        It is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout. The point of
+        using Lorem Ipsum is that it has a more-or-less normal distribution
+      </p>
       <Form onSubmit={handleSubmit}>
         <TextField
           label="Search movies"
