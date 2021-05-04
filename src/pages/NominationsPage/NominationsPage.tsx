94d51e85272ff21116e1c82541a713f715aa170f
Janakitti <janakittiratana@gmail.com>
2021-05-04 19:31:27 -0400

    Read query params on Nominations page

--- src/pages/NominationsPage/NominationsPage.tsx
+++ src/pages/NominationsPage/NominationsPage.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { IMovieMeta } from "../../shared/interfaces";
+import { getMovieDetails } from "../../services/movieservice";
+
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+};
+
 const NominationsPage = () => {
-  return <h1>Nominations</h1>;
+  const [nominations, setNominations] = useState<IMovieMeta[]>([]);
+  const query = useQuery();
+
+  useEffect(() => {
+    const data = query.get("nominations");
+    const ids = data?.split("-");
+    if (ids) {
+      (async () => {
+        const metaResuts = await Promise.all(
+          ids?.map((id: string) => getMovieDetails(id))
+        );
+        setNominations(metaResuts);
+      })();
+    }
+  }, []);
+
+  return <h1>{JSON.stringify(nominations)}</h1>;
 };
 
 export default NominationsPage;
