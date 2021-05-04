e76115454360a3176982af17e9247b9009bccf2c
Janakitti <janakittiratana@gmail.com>
2021-05-04 17:03:12 -0400

    Make delete icon clickable

--- src/components/NominationsCol/NominationCard/NominationCard.tsx
+++ src/components/NominationsCol/NominationCard/NominationCard.tsx
@@ -21,19 +21,19 @@ const NominationCard = ({ movie }: INominationCardProps) => {
   };
 
   return (
-    <Card
-      className="nomination-card__container"
-      bgImage={movie.Poster}
-      tint
-      onClick={removeNomination}
-    >
+    <Card className="nomination-card__container" bgImage={movie.Poster} tint>
       <div className="grid-container">
         <div>
           <h1 className="title">{movie.Title}</h1>
           <h2 className="subtitle">{movie.Year}</h2>
         </div>
         <div className="delete-column">
-          <img src="./delete.svg" alt="Delete nomination" />
+          <img
+            src="./delete.svg"
+            alt="Delete nomination"
+            className="delete-icon"
+            onClick={removeNomination}
+          />
         </div>
       </div>
     </Card>
