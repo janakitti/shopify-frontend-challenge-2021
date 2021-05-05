09565c4fdb893598ef25677748df4738303d2948
Janakitti <janakittiratana@gmail.com>
2021-05-05 18:20:09 -0400

    Add name and button to Nominations page

--- src/pages/NominationsPage/NominationsPage.tsx
+++ src/pages/NominationsPage/NominationsPage.tsx
@@ -4,15 +4,22 @@ import { IMovieMeta } from "../../shared/interfaces";
 import { getMovieDetails } from "../../services/movieservice";
 import SharedNominationItem from "../../components/SharedNominationItem/SharedNominationItem";
 import { isIMovieMeta } from "../../shared/utils";
+import { Button } from "@shopify/polaris";
+import { useHistory } from "react-router-dom";
 
 const useQuery = () => {
   return new URLSearchParams(useLocation().search);
 };
 
 const NominationsPage = () => {
+  let history = useHistory();
   const [nominations, setNominations] = useState<JSX.Element[]>([]);
   const [username, setUsername] = useState("");
   const query = useQuery();
+
+  const handleClick = () => {
+    history.push("/");
+  };
 
   useEffect(() => {
     const data = query.get("data")?.split("-");
@@ -34,9 +41,20 @@ const NominationsPage = () => {
   }, []);
 
   return (
-    <div className="shared-nominations__container">
-      <h1>{username}</h1>
-      {nominations}
+    <div className="shared-nominations__page-wrapper">
+      <div className="shared-nominations__title-container">
+        <div className="shared-nominations__logo-container">
+          <img src="./shopify_logo.svg" className="logo__img" alt="logo" />
+          <h1 className="logo__name">the shoppies</h1>
+        </div>
+        <h1 className="title">{username}'s Nominations</h1>
+      </div>
+      <div className="shared-nominations__container">{nominations}</div>
+      <div className="shared-nominations__button-container">
+        <Button primary size="slim" onClick={handleClick}>
+          Cast your own nominations!
+        </Button>
+      </div>
     </div>
   );
 };
