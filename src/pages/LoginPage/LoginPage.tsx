4abe80577b2c7879fdf9738dd1788ffda8a5725e
Janakitti <janakittiratana@gmail.com>
2021-05-04 22:22:33 -0400

    Add handleLogin

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,22 +1,52 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import CustomCard from "../../components/Card/Card";
-import { TextField, Button, Form } from "@shopify/polaris";
+import { TextField, Button, Form, InlineError } from "@shopify/polaris";
+import { USER_PASSWORD } from "../../shared/constants";
+import { useHistory } from "react-router-dom";
 
 const LoginPage = () => {
+  let history = useHistory();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [erroMsg, setErrorMsg] = useState("");
+
+  const handleLogin = useCallback(
+    (_event) => {
+      if (!username || !password) {
+        setErrorMsg("Please enter your credentials.");
+        return;
+      } else if (password === USER_PASSWORD) {
+        history.push("/search");
+      } else {
+        setErrorMsg("Username or password is incorrect.");
+      }
+    },
+    [username, password]
+  );
+
   return (
     <div className="login-card-container">
       <CustomCard className="login-card">
         <h1>the shoppies</h1>
         <h2>Sign in to nominate</h2>
-        <Form onSubmit={() => {}}>
+        <Form onSubmit={handleLogin}>
           <div className="form-container">
-            <TextField label="Username" value="" onChange={() => {}} />
+            <TextField
+              label="Username"
+              value={username}
+              onChange={setUsername}
+            />
             <TextField
               label="Password"
               type="password"
-              value=""
-              onChange={() => {}}
+              value={password}
+              onChange={setPassword}
+              aria-describedby="password-field"
             />
+            {erroMsg && (
+              <InlineError message={erroMsg} fieldID="password-field" />
+            )}
+
             <Button primary fullWidth submit>
               Log in
             </Button>
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,2 +1,3 @@
 export const OMDB_API_URL = "http://www.omdbapi.com";
 export const NOMINATION_NUMBER = 5;
+export const USER_PASSWORD = "shoppies";
