3114825d3a1fcf85d749b8f1a152c89b34f1df1a
Janakitti <janakittiratana@gmail.com>
2021-05-04 21:51:09 -0400

    Create LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { AppProvider } from "@shopify/polaris";
 import enTranslations from "@shopify/polaris/locales/en.json";
 
+import LoginPage from "./pages/LoginPage/LoginPage";
 import HomePage from "./pages/HomePage/HomePage";
 import NominationsPage from "./pages/NominationsPage/NominationsPage";
 import StateProvider from "./AppContext";
@@ -17,7 +18,8 @@ function App() {
         <Router>
           <AppProvider i18n={enTranslations}>
             <Switch>
-              <Route exact path="/" component={HomePage} />
+              <Route exact path="/" component={LoginPage} />
+              <Route exact path="/search" component={HomePage} />
               <Route path="/nominations" component={NominationsPage} />
             </Switch>
           </AppProvider>
---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+
+const LoginPage = () => {
+  return <div>hello</div>;
+};
+
+export default LoginPage;
